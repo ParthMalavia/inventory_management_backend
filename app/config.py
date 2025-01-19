47443e4d41a56e933b9494df96b96e11101524d6
Parth Malavia <parthmalavia286@gmail.com>
2025-01-19 18:58:50 +0530

    updated sample env

--- app/config.py
+++ app/config.py
@@ -1,6 +1,9 @@
 import os
 from pydantic_settings import BaseSettings
+from dotenv import load_dotenv
 
+
+load_dotenv()
 
 class Settings(BaseSettings):
     SECRET_KEY: str = os.getenv("SECRET_KEY", "your-secret-key")
@@ -10,8 +13,8 @@ class Settings(BaseSettings):
     DB_PASSWORD: str = os.getenv("DB_PASSWORD", "password")
     DB_NAME: str = os.getenv("DB_NAME", "inventory")
 
-    class Config:
-        env_file = ".env"
+    # class Config:
+    #     env_file = ".env"
 
 
 settings = Settings()
